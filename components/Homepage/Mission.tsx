1947d192c13f3d8569923d7dc5af549ed2a338f1
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-07-08 17:52:27 +0100

    refactor: Update Mission component layout and add rounded border

--- components/Homepage/Mission.tsx
+++ components/Homepage/Mission.tsx
@@ -3,11 +3,12 @@ import React from "react";
 
 const Mission = () => {
   return (
-    <div className="flex justify-center gap-20 items-start px-20 py-16">
+    <div className="flex flex-row-reverse justify-center gap-20 items-start px-20 py-16">
       <Image
         alt={"mission"}
         width={600}
         height={1000}
+        className=" rounded-br-[3rem]"
         src={
           "https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
