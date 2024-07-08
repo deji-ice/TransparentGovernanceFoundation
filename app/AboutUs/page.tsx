6bb718660cdeb8b1cef68a79097cb7eac9757da5
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-07-08 17:54:02 +0100

    refactor: Update AboutUs page with rounded top-left image corner

--- app/AboutUs/page.tsx
+++ app/AboutUs/page.tsx
@@ -19,6 +19,7 @@ const AboutUs = () => {
             alt="kk"
             width={584}
             height={289}
+            className="rounded-tl-[3rem]"
             src={
               "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?q=80&w=1484&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             }
