5dac313dd9bcdf0a3fda5db09905adf8a0177f8a
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-07-11 00:08:44 +0100

    refactor: Update Modal component styling and functionality

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -15,8 +15,8 @@ const Modal = ({
   return (
     <>
       {showModal && (
-        <div className="fixed top-0 left-0 w-screen h-screen bg-green-500/50 flex items-center justify-center">
-          <div className="flex gap-2 bg-white rounded-lg relative">
+        <div className="fixed top-0 hover:cursor-no-drop left-0 w-screen h-screen bg-green-500/20 flex items-center justify-center">
+          <div className="flex cursor-default gap-2 top-10 bg-white rounded-lg relative">
             <button
               onClick={() => setShowModal(false)}
               className="btn h-12 w-12 bg-white text-green-500 rounded-full font-semibold btn-sm btn-circle btn-ghost absolute right-[-15px] top-[-10px]"
@@ -25,17 +25,17 @@ const Modal = ({
             </button>
 
             <div>
-              <Image width={200} src={data.pic ?? ""} alt={""} height={300} />
+              <Image width={250} src={data.pic ?? ""} alt={""} height={300} />
             </div>
-            <div className="flex flex-col gap-10 justify-between px-10">
-              <span className="text-lg flex flex-col  ">
-                <p className="font-semibold transition-all duration-500 ease-in">
-                  {data.title}
+            <div className="flex flex-col gap-10 justify-between px-10 py-10">
+              <span className="text-sm flex flex-col  ">
+                <p className="font-semibold text-lg transition-all duration-500 ease-in">
+                  {data.name}
                 </p>
-                <p> {data.name}</p>
+                <p> {data.title}</p>
               </span>
               <div className="h-[20rem] w-[20rem] overflow-y-scroll ">
-                <p className="text-sm">{data.bio}</p>
+                <p className="text-xs tracking-wide leading-5 px-2">{data.bio}</p>
               </div>
             </div>
           </div>
