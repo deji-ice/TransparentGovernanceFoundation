010787794a60c072583af6f915494e8b5655829f
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-07-11 17:58:36 +0100

    refactor: Add JoinUs component to Membership page

--- app/Membership/page.tsx
+++ app/Membership/page.tsx
@@ -1,13 +1,14 @@
 import Executives from "@/components/Membership/Executives";
+import JoinUs from "@/components/Membership/JoinUs";
 import { leaders } from "@/lib/data";
 import React from "react";
 
 const Membership = () => {
   return (
-    <div className=" flex flex-col items-center justify-between mt-20 px-20">
-      <h2 className="mb-14 font-playfair text-2xl font-semibold">Meet Our Visionaries Members</h2>
+    <div className=" flex flex-col w-screen overflow-x-clip items-center justify-between mt-24 px-20">
+      <h2 className="mb-14 font-playfair text-3xl font-semibold">Meet Our Visionaries Members</h2>
       <div className="flex flex-col items-center gap-12">
-        <p className="px-24">
+        <p className="px-32">
           At the Transparent Governance Foundation (TGF), our members are the
           heart and soul of our mission to promote good governance,
           accountability, and social justice. We are proud to introduce you to
@@ -17,7 +18,7 @@ const Membership = () => {
           society.
         </p>
         <div>
-          <p className="px-24 mb-10">
+          <p className="px-32 mb-16">
             While the TGF foundation members include the following visionaries,
             we hope to grow our membership for effectiveness. The following are
             the visionary members with their portfolios and brief background
@@ -26,6 +27,9 @@ const Membership = () => {
           <div className=" h-full flex flex-col items-center">
           <Executives leaders={leaders} />
           </div>
+        </div>
+        <div>
+          <JoinUs />
         </div>
       </div>
     </div>
--- components/Membership/Executives.tsx
+++ components/Membership/Executives.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Leader } from "@/lib/data";
 import Image from "next/image";
 import React, { useState } from "react";
@@ -6,21 +6,21 @@ import { FaArrowRight } from "react-icons/fa6";
 import Modal from "../Modal";
 
 const Executives = ({ leaders }: { leaders: Leader[] }) => {
-    const [showModal, setShowModal] = useState(false);
-    const [userData, setUserData] = useState<Leader | null>(null);
+  const [showModal, setShowModal] = useState(false);
+  const [userData, setUserData] = useState<Leader | null>(null);
 
-    const handleToggle = (i) => {
-      setShowModal(!showModal);
-      setUserData(i);
-      console.log(showModal,"showModal")
-    }
+  const handleToggle = (i: React.SetStateAction<Leader | null>) => {
+    setShowModal(!showModal);
+    setUserData(i);
+    console.log(showModal, "showModal");
+  };
   return (
     <div className="relative flex flex-col items-center">
       <h3 className="absolute top-[-19px] bg-white px-5 text-2xl font-semibold font-playfair">
         The Executives
       </h3>
       <hr className="w-screen border-b border-b-black" />
-      <div className="mt-10 grid grid-cols-4 mb-10 gap-10 h-full ">
+      <div className="mt-16 grid grid-cols-4  justify-center mb-10 gap-10 h-full ">
         {leaders.map((item: Leader) => (
           <div
             key={item.id}
@@ -29,8 +29,11 @@ const Executives = ({ leaders }: { leaders: Leader[] }) => {
             <div className="transition duration-300 ease-in-out transform hover:scale-105">
               <div className="group-hover:border-slate-100 border-transparent absolute top-4 left-1/2 transform -translate-x-1/2  h-44 w-44 border-2 rounded-full "></div>
               <div
-                  onClick={()=>{handleToggle(item)}}
-              className="w-full h-[300px] absolute bg-transparent group-hover:bg-green-900  opacity-30 rounded-none transition-all duration-700 ease-in-out group-hover:rounded-2xl"></div>
+                onClick={() => {
+                  handleToggle(item);
+                }}
+                className="w-full h-[300px] absolute bg-transparent group-hover:bg-green-900  opacity-30 rounded-none transition-all duration-700 ease-in-out group-hover:rounded-2xl"
+              ></div>
               <FaArrowRight className=" group-hover:bg-white group-hover:text-green-500 rounded-full text-xs p-4 h-12 w-12 text-transparent bg-transparent absolute top-3/4 right-2 transform -translate-y-1/2 transition-all duration-500 ease-in-out" />
               <Image
                 width={200}
@@ -47,9 +50,12 @@ const Executives = ({ leaders }: { leaders: Leader[] }) => {
               </p>
               <p> {item.name}</p>
             </span>
-            <Modal data={userData} showModal={showModal}  setShowModal={setShowModal}/>
+            <Modal
+              data={userData}
+              showModal={showModal}
+              setShowModal={setShowModal}
+            />
           </div>
-          
         ))}
       </div>
     </div>
