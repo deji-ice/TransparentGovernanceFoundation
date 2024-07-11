ff557d134b9cfb140bc3e80c47333799ea34db4b
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-07-11 17:57:35 +0100

    refactor: Add JoinUs component to Membership page

---
+++ components/Membership/JoinUs.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const JoinUs = () => {
+  return (
+    <div className="flex flex-col items-center px-32 my-10 gap-10">
+      <h3 className="font-playfair text-2xl font-semibold">Join Us</h3>
+      <p>
+        Our members are dedicated professionals who are passionate about making
+        a difference. If you share our commitment to good governance,
+        transparency, and social justice, we invite you to join us.<br/> Visit our
+        Membership Page to learn more about how you can become a part of TGF and
+        contribute to our mission.
+
+      </p>
+      <span className="italic  text-green-600 font-semibold">
+          Transparent Governance Foundation – Together, We Can Make a Difference
+        </span>
+    </div>
+  );
+};
+
+export default JoinUs;
