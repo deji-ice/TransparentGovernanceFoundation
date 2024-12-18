4832847c99905107cba5bd4e8a99ade33b5b2d10
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-12-18 13:20:13 +0100

    feat: Add About Us page with mission statement and organizational overview

---
+++ app/WhoWeAre/page.tsx
@@ -0,0 +1,61 @@
+import Mission from "@/components/Homepage/Mission";
+import { IoLocationOutline } from "react-icons/io5";
+import React from "react";
+import Image from "next/image";
+import Introduction from "@/components/Homepage/Introduction";
+
+const AboutUs = () => {
+  return (
+    <div className="flex flex-col items-center justify-between">
+      <section className="flex flex-col items-center px-10 md:px-20  bg-white">
+        <h2 className="text-2xl md:text-3xl font-bold  font-playfair text-center text-black">
+          Who We Are
+        </h2>
+
+        <div className="flex gap-10 items-center">
+          <Image
+            alt="kk"
+            width={584}
+            height={289}
+            className="rounded-tl-[3rem]"
+            src={
+              "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?q=80&w=1484&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            }
+          />
+          <div className="flex flex-col items-start ">
+            <p className="mb-3 text-start text-black max-w-6xl">
+              The Transparent Governance Foundation is a Non-Governmental
+              Organization dedicated to promoting good governance as a
+              cornerstone for sustainable progress, social harmony, and
+              inclusive development.
+            </p>
+            <p className="mb-3 text-start text-black max-w-6xl">
+              We advance principles of transparency, accountability, and ethical
+              leadership, recognizing the critical role of governance in shaping
+              nations and communities.
+            </p>
+            <p className="mb-3 text-start text-black max-w-6xl">
+              Our mission is to strengthen institutions and empower citizens to
+              participate meaningfully in decision-making, regardless of their
+              background.
+            </p>
+            <p className="text-start text-black max-w-6xl">
+              Inspired by diverse cultures, we strive to foster a society where
+              justice, fairness, and respect for human rights prevail.
+            </p>
+          </div>
+        </div>
+
+        {/* <p className="text-center text-black max-w-6xl">
+          Drawing inspiration from the collective wisdom of diverse cultures and
+          civilizations, the Foundation aspires to foster an enlightened and
+          harmonious society where the principles of justice, fairness, and
+          respect for human rights prevail.
+        </p> */}
+      </section>
+      {/* <Mission /> */}
+    </div>
+  );
+};
+
+export default AboutUs;
