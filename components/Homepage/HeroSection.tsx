33fb6229bcd39594e950b9612ec0a25504c8dbd1
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-12-19 15:02:53 +0100

    feat: Enhance HeroSection layout with improved image opacity and typography adjustments

--- components/Homepage/HeroSection.tsx
+++ components/Homepage/HeroSection.tsx
@@ -5,16 +5,16 @@ import React from "react";
 const HeroSection = () => {
   return (
     <div className="relative bg-green-800 flex flex-col items-center justify-center gap-5 px-4 md:px-[10%] h-[94dvh] w-screen text-white">
-      {/* <Image
-        className="absolute top-0 z-20 h-full w-[70rem] md:w-[50rem] right-0"
+      <Image
+        className="absolute top-0  h-full w-[70rem] md:w-[50rem] right-0 object-cover opacity-50"
         src={
           "https://res.cloudinary.com/dhvwthnzq/image/upload/v1720023999/Transparent%20Governance%20Foundation/wave_nvvvyj.png"
         }
         alt={"wave"}
         height={1000}
         width={500}
-      /> */}
-      <p className="font-semibold text-center font-dm_sans md:text-xl">
+      />
+      <p className="font-semibold text-center font-dm_sans italic md:text-xl">
         Empowering Communities for a Brighter Future
       </p>
       <div className="flex flex-col  gap-3 md:gap-8 justify-center items-center m">
@@ -32,7 +32,7 @@ const HeroSection = () => {
           Donate Now
         </button>
 
-        <button className="border-white border-2 bg-green-800 px-6 py-2">
+        <button className="border-white border-2 relative z-10 bg-green-800 px-6 py-2">
           <Link href="/who-we-are">Know About Us</Link>
         </button>
       </div>
