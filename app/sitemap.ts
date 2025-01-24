c3f256a25f55a49dbe8d6b5a5b5bb919edbb2cac
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-01-24 11:03:18 +0100

    feat: Update sitemap URLs and change frequencies for improved navigation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from 'next'
- 
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
@@ -9,16 +9,28 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: 'https://transparentgovernancefoundation.org/about',
+      url: 'https://transparentgovernancefoundation.org/who-we-are',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://transparentgovernancefoundation.org/blog',
+      url: 'https://transparentgovernancefoundation.org/projects-program',
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: 'monthly',
       priority: 0.5,
     },
+    {
+      url: 'https://transparentgovernancefoundation.org/contact-us',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://transparentgovernancefoundation.org/memberships',
+      lastModified: new Date(),
+      changeFrequency: 'always',
+      priority: 0.5,
+    }
   ]
 }
