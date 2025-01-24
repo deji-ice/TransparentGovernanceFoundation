e7c3d40ef605652dc3b33c68fe6dc81050340288
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-01-24 11:42:04 +0100

    feat: Update sitemap and robots.txt URLs to include 'www' for consistency

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,31 +3,31 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://transparentgovfoundation.org',
+      url: 'https://www.transparencegovfoundation.org',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://transparentgovfoundation.org/who-we-are',
+      url: 'https://www.transparencegovfoundation.org/who-we-are',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://transparentgovfoundation.org/projects-program',
+      url: 'https://www.transparencegovfoundation.org/projects-program',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.5,
     },
     {
-      url: 'https://transparentgovfoundation.org/contact-us',
+      url: 'https://www.transparencegovfoundation.org/contact-us',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.5,
     },
     {
-      url: 'https://transparentgovfoundation.org/memberships',
+      url: 'https://www.transparencegovfoundation.org/memberships',
       lastModified: new Date(),
       changeFrequency: 'always',
       priority: 0.5,
